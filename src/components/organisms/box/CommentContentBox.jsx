95cb27eff941f3d8d200fef37d587c305fddc4dc
KimKyuHoi <k546kh@gmail.com>
2023-11-27 16:44:59 +0900

    [Feat]: 날짜 전처리해서 구현하기

--- src/components/organisms/box/CommentContentBox.jsx
+++ src/components/organisms/box/CommentContentBox.jsx
@@ -4,12 +4,23 @@ import styled from "styled-components";
 
 const CommentContentBox = ({ content, date, author }) => {
 
+    const originalDate = date;
+    const regex = /^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2})/;
+    const match = originalDate.match(regex);
+
+    if (match) {
+        const extractedDate = match[1];
+        console.log(extractedDate);
+    } else {
+        console.log("일치하는 패턴을 찾을 수 없습니다.");
+    }
+
     return (
         <CommentBox>
             <CommentWrapper>
                 <Smalltitle content={author.email} />
                 <Smalltitle content={content} />
-                <Smalltitle content={date} />
+                <Smalltitle content={match[1]} />
             </CommentWrapper>
         </CommentBox>
     )
