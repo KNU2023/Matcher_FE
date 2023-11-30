4b9435e15f8fae9f231a40f8ac2eb759350afc63
KimKyuHoi <k546kh@gmail.com>
2023-12-01 01:49:19 +0900

    [Feat]: 마이페이지 jobPost 조회 및 삭제 구현

--- src/components/organisms/box/MyPageJobContentBox.jsx
+++ src/components/organisms/box/MyPageJobContentBox.jsx
@@ -1,14 +1,48 @@
 import MyPageContentBox from "../../molecules/box/MyPageContentBox";
 import TitleboxModalSecondText from "../../molecules/text/TitleboxModalSecondText";
 import styled from "styled-components";
+import { MdOutlineDelete } from "react-icons/md";
+import { VscGitPullRequestGoToChanges } from "react-icons/vsc";
 
 const MyPageJobContentBox = ({ title, date, jobPostId, author }) => {
+
+    const onClickChange = () => {
+        confirm('정말로 수정하시겠습니까?');
+    }
+
+    const onClickDelete = () => {
+        confirm('정말로 삭제하시겠습니까?');
+        if (confirm) {
+            try {
+                const accessToken = localStorage.getItem("accessToken");
+                const response = axios.delete(`/api/jobpost/${jobPostId}`, {
+                    headers: {
+                        'Authorization': accessToken,
+                    },
+                });
+
+                console.log(response.data);
+                alert("삭제되었습니다.")
+                window.location.reload();
+            }
+            catch (error) {
+                //throw new Error(`예약 데이터를 가져오는 중 오류 발생: ${error.message}`);
+                console.log(error);
+            }
+        }
+    }
+
     return (
         <>
             <MyPageContentBox>
                 <ContentWrapper>
                     <TitleboxModalSecondText content={title} size="8" weight="bold" />
                     <TitleboxModalSecondText content={date} size="8" weight="bold" />
+                    <TitleboxModalSecondText content={author.name} size="8" weight="bold" />
+                    <IconWrapper>
+                        <VscGitPullRequestGoToChanges size="24" cursor="pointer" color="#03C75A" onClick={onClickChange} />
+                        <MdOutlineDelete size="24" cursor="pointer" color="#03C75A" onClick={onClickDelete} />
+                    </IconWrapper>
                 </ContentWrapper>
             </MyPageContentBox>
         </>
@@ -20,6 +54,11 @@ const ContentWrapper = styled.div`
     flex-direction: row;
     justify-content: space-between;
     
+`;
+
+const IconWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
 `;
 
 export default MyPageJobContentBox;
